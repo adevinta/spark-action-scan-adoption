9d5c031a9a356881bd65b18a5fdd08973331e090
andresin87 <andresin87@gmail.com>
2024-05-27 18:50:48 +0200

    fix(action): bump

--- src/main.mjs
+++ src/main.mjs
@@ -1,11 +1,11 @@
 import * as core from '@actions/core'
 import { exec } from '@actions/exec'
-import * as github from '@actions/github'
-import { defaults as defaultGitHubOptions } from '@actions/github/lib/utils.js'
-import { requestLog } from '@octokit/plugin-request-log'
-import { retry } from '@octokit/plugin-retry'
+// import * as github from '@actions/github'
+// import { defaults as defaultGitHubOptions } from '@actions/github/lib/utils.js'
+// import { requestLog } from '@octokit/plugin-request-log'
+// import { retry } from '@octokit/plugin-retry'
 
-import { getRetryOptions, parseNumberArray } from './retry-options.mjs'
+// import { getRetryOptions, parseNumberArray } from './retry-options.mjs'
 
 let output = ''
 let error = ''
@@ -15,36 +15,33 @@ let error = ''
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function main() {
-  const token = core.getInput('github-token', { required: true })
+  // const token = process.env.GITHUB_TOKEN
   const debug = process.env.DEBUG === 'true'
-  const config = process.env.CONFIG
-  const userAgent = core.getInput('user-agent')
-  const previews = core.getInput('previews')
-  const retries = parseInt(core.getInput('retries'))
-  const exemptStatusCodes = parseNumberArray(core.getInput('retry-exempt-status-codes'))
+  const configuration = process.env.CONFIGURATION
+  // const userAgent = core.getInput('user-agent')
+  // const previews = core.getInput('previews')
+  // const retries = parseInt(core.getInput('retries'))
+  // const exemptStatusCodes = parseNumberArray(core.getInput('retry-exempt-status-codes'))
 
-  const [retryOpts, requestOpts] = getRetryOptions(retries, exemptStatusCodes, defaultGitHubOptions)
-
-  const opts = {
-    log: debug ? console : undefined,
-    userAgent: userAgent || undefined,
-    previews: previews ? previews.split(',') : undefined,
-    retry: retryOpts,
-    request: requestOpts,
-  }
-
-  const gh = github.getOctokit(token, opts, retry, requestLog)
-
-  // eslint-disable-next-line no-console
-  console.log(gh)
-
-  const whoToGreet = core.getInput('who-to-greet', { required: true })
-  core.info(`Hello, ${whoToGreet}!`)
+  // const [retryOpts, requestOpts] = getRetryOptions(retries, exemptStatusCodes, defaultGitHubOptions)
+  //
+  // const opts = {
+  //   log: debug ? console : undefined,
+  //   userAgent: userAgent || undefined,
+  //   previews: previews ? previews.split(',') : undefined,
+  //   retry: retryOpts,
+  //   request: requestOpts,
+  // }
+  //
+  // const gh = github.getOctokit(token, opts, retry, requestLog)
+  //
+  // // eslint-disable-next-line no-console
+  // console.log(gh)
 
   // eslint-disable-next-line no-console
   console.log('debug:', debug)
   // eslint-disable-next-line no-console
-  console.log('config:', config)
+  console.log('config:', configuration)
 
   const options = {
     listeners: {
